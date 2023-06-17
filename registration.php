d36be790e771dd2e2995b62d2d6b0ccf27a20699
gozonjoedaimar <gozonjoedaimar@gmail.com>
2023-06-17 02:09:17 +0000

    First commit

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Clothingline/base', __DIR__);
